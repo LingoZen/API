e48b6241d46c2f4b3a16644e6e6dc2bc694dc62d
Nickolas Freitas <n_freitas1@salemstate.edu>
2017-02-22 23:56:52 -0500

    add remaining query and mutations to gql root schema

--- gql-root-schema.js
+++ gql-root-schema.js
@@ -3,31 +3,52 @@ import {
     GraphQLObjectType
 } from 'graphql';
 
-import {queryFields as UserQueryFields} from './users/gql-query-fields';
-import {mutationFields as UserMutationFields} from './users/gql-mutation-fields';
+import {queryFields as CommentQueryFields} from './comments/gql-query-fields';
+import {mutationFields as CommentMutationFields} from './comments/gql-mutation-fields';
+
+import {queryFields as LanguageQueryFields} from './languages/gql-query-fields';
+import {mutationFields as LanguageMutationFields} from './languages/gql-mutation-fields';
+
+import {queryFields as ReactionQueryFields} from './reactions/gql-query-fields';
+import {mutationFields as ReactionMutationFields} from './reactions/gql-mutation-fields';
+
 import {queryFields as SourceSentenceQueryFields} from './source-sentences/gql-query-fields';
 import {mutationFields as SourceSentenceMutationFields} from './source-sentences/gql-mutation-fields';
+
+import {queryFields as TranslationQueryFields} from './translations/gql-query-fields';
+import {mutationFields as TranslationMutationFields} from './translations/gql-mutation-fields';
+
+import {queryFields as UserQueryFields} from './users/gql-query-fields';
+import {mutationFields as UserMutationFields} from './users/gql-mutation-fields';
 
 export const rootSchema = new GraphQLSchema({
     query: new GraphQLObjectType({
         name: `Query`,
-        description: `User Query`,
+        description: `Root Query`,
         fields: () => {
             return Object.assign(
                 {},
-                UserQueryFields,
-                SourceSentenceQueryFields
+                CommentQueryFields,
+                LanguageQueryFields,
+                ReactionQueryFields,
+                SourceSentenceQueryFields,
+                TranslationQueryFields,
+                UserQueryFields
             );
         }
     }),
     mutation: new GraphQLObjectType({
         name: `Mutation`,
-        description: `User Mutations`,
+        description: `Root Mutations`,
         fields () {
             return Object.assign(
                 {},
-                UserMutationFields,
-                SourceSentenceMutationFields
+                CommentMutationFields,
+                LanguageMutationFields,
+                ReactionMutationFields,
+                SourceSentenceMutationFields,
+                TranslationMutationFields,
+                UserMutationFields
             );
         }
     })
