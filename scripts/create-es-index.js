fef4a89fc08ce092f4520d069dd30db3bfbfd761
NickFreitas <n_freitas1@salemstate.edu>
2017-02-28 20:51:59 -0500

    add create es index script

---
+++ scripts/create-es-index.js
@@ -0,0 +1,78 @@
+import async from 'async';
+
+import {esConnection} from '../es';
+import {config} from '../config';
+import {mapping as frenchSourceSentencesMapping} from '../source-sentences/es-mappings/french';
+import {mapping as englishSourceSentencesMapping} from '../source-sentences/es-mappings/english';
+import {mapping as spanishSourceSentencesMapping} from '../source-sentences/es-mappings/spanish';
+
+const mappings = {
+    french: frenchSourceSentencesMapping,
+    english: englishSourceSentencesMapping,
+    spanish: spanishSourceSentencesMapping
+};
+
+let indexExists = {};
+
+async.series([
+    (next) => {
+        async.each(config.languages, (language, nextLanguage) => {
+            esConnection.indices.exists({
+                index: `${config.es.sourceSentenceIndexPrefix}${language}`
+            }).then((_indexExists) => {
+                indexExists[language] = _indexExists;
+
+                return nextLanguage();
+            }).catch((err) => next(err));
+        }, (err) => {
+            if (err) {
+                return next(err);
+            }
+
+            return next();
+        });
+    },
+    (next) => {
+        async.each(config.languages, (language, nextLanguage) => {
+            if (!indexExists[language]) {
+                return nextLanguage();
+            }
+
+            esConnection.indices.delete({
+                index: `${config.es.sourceSentenceIndexPrefix}${language}`
+            }).then(() => nextLanguage()).catch((err) => nextLanguage(err));
+        }, (err) => {
+            if (err) {
+                return next(err);
+            }
+
+            return next();
+        });
+    },
+    (next) => {
+        async.each(config.languages, (language, nextLanguage) => {
+            if (!mappings[language]) {
+                return nextLanguage(new Error(`No mapping found for ${language}`));
+            }
+
+            esConnection.indices.create({
+                index: `${config.es.sourceSentenceIndexPrefix}${language}`,
+                body: {
+                    mappings: mappings[language]
+                }
+            }).then(() => nextLanguage()).catch((err) => nextLanguage(err));
+        }, (err) => {
+            if (err) {
+                return next(err);
+            }
+
+            return next();
+        });
+    }
+], (err) => {
+    if (err) {
+        return console.error(err);
+    }
+
+    return console.log('Created index successfully');
+});
