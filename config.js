2b80721e563e93c09dac2ee9c1d81ee5a1a9e6e1
NickFreitas <n_freitas1@salemstate.edu>
2017-02-28 20:54:46 -0500

    add elasticsearch config parameters

--- config.js
+++ config.js
@@ -1,11 +1,23 @@
 export const config = {
     server: {
         portNumber: 3000,
-        host: 'localhost'
+        host: `localhost`
     },
     db: {
-        name: 'lingozen',
-        host: 'localhost',
-        dialect: 'mysql'
-    }
+        name: `lingozen`,
+        host: `localhost`,
+        dialect: `mysql`
+    },
+    es: {
+        host: `localhost:9200`,
+        index: `lingozen`,
+        log: `trace`,
+        sourceSentenceIndexPrefix: `source-sentences-`,
+        sourceSentenceType: `source-sentence`
+    },
+    languages: [
+        `english`,
+        `french`,
+        `spanish`
+    ]
 };
